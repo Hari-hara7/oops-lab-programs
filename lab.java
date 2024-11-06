ded75730676a9487c3ded220a0def44dd4b2d44d
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2024-11-06 22:38:52 +0530

    Update lab.java

--- lab.java
+++ lab.java
@@ -1,1 +1,45 @@
-hgbdhfhhhfghjg
+package oopslab;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+public class lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   Random random=new Random();
+   int randomNumber=random.nextInt(Integer.MAX_VALUE)+1;
+   
+   Scanner scanner=new Scanner(System.in);
+   int guess=0;
+   
+   System.out.println("Welcome to guess my number game");
+   System.out.println("i thinking a number");
+   
+   
+   while(guess!=randomNumber) {
+	   System.out.println("Enter your guess:");
+	   guess=scanner.nextInt();
+	   
+	   
+	   if(guess>randomNumber) {
+		   
+		   System.out.println("Higher try again");
+	   }
+	   else if(guess<randomNumber){
+		   System.out.println("lower! try again");
+	   }
+	   else {
+		   System.out.println("congratulation u  won the game");
+	   }
+   }
+    
+   
+   scanner.close();
+	}
+
+}
+
