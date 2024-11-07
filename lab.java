4b1284b80819e4daafd8a9fc663c028e3fc740da
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2024-11-07 19:43:01 +0530

    Update lab.java

--- lab.java
+++ lab.java
@@ -1,45 +1,33 @@
 package oopslab;
 
-
 import java.util.Random;
 import java.util.Scanner;
 
+public class Lab1 {
 
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(Integer.MAX_VALUE) + 1;
 
-public class lab1 {
+        Scanner scanner = new Scanner(System.in);
+        int guess = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-   Random random=new Random();
-   int randomNumber=random.nextInt(Integer.MAX_VALUE)+1;
-   
-   Scanner scanner=new Scanner(System.in);
-   int guess=0;
-   
-   System.out.println("Welcome to guess my number game");
-   System.out.println("i thinking a number");
-   
-   
-   while(guess!=randomNumber) {
-	   System.out.println("Enter your guess:");
-	   guess=scanner.nextInt();
-	   
-	   
-	   if(guess>randomNumber) {
-		   
-		   System.out.println("Higher try again");
-	   }
-	   else if(guess<randomNumber){
-		   System.out.println("lower! try again");
-	   }
-	   else {
-		   System.out.println("congratulation u  won the game");
-	   }
-   }
-    
-   
-   scanner.close();
-	}
+        System.out.println("Welcome to the 'Guess My Number' game!");
+        System.out.println("I'm thinking of a number between 1 and " + Integer.MAX_VALUE);
 
+        while (guess != randomNumber) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+
+            if (guess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else if (guess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else {
+                System.out.println("Congratulations! You've guessed the number!");
+            }
+        }
+
+        scanner.close();
+    }
 }
-
